5ad7663892d6ca77d1fbda6d43ad1059443a9949
Evgeny Cherkashin <eugeneai@irnok.net>
2018-10-31 23:35:46 +0800

    Added test of one docx2html converter.

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,7 +1,15 @@
 from nose.plugins.skip import SkipTest
 from nose.tools import assert_raises, nottest
 
-#@SkipTest
+from docx2html import convert
+import os.path
+
+INPUT = os.path.abspath('input/query1.docx')
+OUTPUT = os.path.abspath('input/query1-2.html')
+
+# @SkipTest
+
+
 class TestBasic:
 
     def setUp(self):
@@ -9,6 +17,12 @@ class TestBasic:
 
     def test_something(self):
         assert 1 + 1 == 2
+
+    def test_convert(self):
+        html = convert(INPUT)
+        o = open(OUTPUT, 'w')
+        o.write(html)
+        o.close()
 
     def tearDown(self):
         pass
