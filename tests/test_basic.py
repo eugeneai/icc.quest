3331e49931259796e8ceb8cae8122fe72160a434
Evgeny Cherkashin <eugeneai@irnok.net>
2018-11-01 23:57:16 +0800

    Changed to accept etree form.

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,5 +1,6 @@
 from nose.plugins.skip import SkipTest
 from nose.tools import assert_raises, nottest
+from lxml import etree
 
 from docx2html import convert
 import os.path
@@ -19,9 +20,11 @@ class TestBasic:
         assert 1 + 1 == 2
 
     def test_convert(self):
-        html = convert(INPUT)
+        html = convert(INPUT, charset_metadata=True,
+                       pretty_print=True, as_tree=True)
+        print(html)
         o = open(OUTPUT, 'w')
-        o.write(html)
+        o.write(etree.tostring(html, pretty_print=True, encoding=str))
         o.close()
 
     def tearDown(self):
